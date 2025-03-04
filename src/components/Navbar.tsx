444bd104474928c5070d4a45bccd05fdb27a9c4f
XAVIER Clément Antoine <clement-antoine.xavier@epitech.eu>
2025-03-04 10:25:55 +0400

    Feat: Update the Navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -5,7 +5,35 @@ type NavbarProps = {
 export default function Navbar({ title }: NavbarProps) {
   return (
     <div className="navbar bg-base-100 shadow-sm">
-      <a className="btn btn-ghost text-xl">{title}</a>
+      <div className="navbar-start">
+        <div className="dropdown">
+          <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"> <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /> </svg>
+          </div>
+          <ul
+            tabIndex={0}
+            className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
+            <li><a href="/calendars">Calendars</a></li>
+            <li><a href="/emails">Emails</a></li>
+            <li><a href="/files">Files</a></li>
+            <li><a href="/messages">Messages</a></li>
+            <li><a href="/projects">Projects</a></li>
+          </ul>
+        </div>
+        <a className="btn btn-ghost text-xl">{title}</a>
+      </div>
+      <div className="navbar-center hidden lg:flex">
+        <ul className="menu menu-horizontal px-1">
+          <li><a href="/calendars">Calendars</a></li>
+          <li><a href="/emails">Emails</a></li>
+          <li><a href="/files">Files</a></li>
+          <li><a href="/messages">Messages</a></li>
+          <li><a href="/projects">Projects</a></li>
+        </ul>
+      </div>
+      <div className="navbar-end">
+        {/* TODO: Put profil here ... */}
+      </div>
     </div>
   );
 }
